a5e650d481af3e70c0024327298562a6eb51c907
Hyunmyung Jung <athena214@naver.com>
2025-06-10 23:05:14 +0900

    upload project

---
+++ script.js
@@ -0,0 +1,133 @@
+const list = document.getElementById("list");
+const createBtn = document.getElementById("create-btn");
+
+let todos = [];
+
+createBtn.addEventListener('click', createNewTodo);
+
+function createNewTodo() {
+
+    const item = {
+        id : new Date().getTime(),
+        text : "",
+        complete : false
+    }
+
+    todos.unshift(item);
+
+    const { itemEl, inputEl } = createTodoElment(item);
+
+    list.prepend(itemEl);
+
+    inputEl.removeAttribute("disabled");
+
+    inputEl.focus();
+
+    saveToLocalStorage();
+}
+
+function createTodoElment(item) {
+
+    // 요소 생성
+    const itemEl = document.createElement("div");
+    itemEl.classList.add("item");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = item.complete;
+
+    if(item.complete) {
+        itemEl.classList.add("complete");
+    }
+
+    const inputEl = document.createElement("input");
+    inputEl.type = "text";
+    inputEl.value = item.text;
+    inputEl.setAttribute("disabled", "");
+
+    const actionsEl = document.createElement("div");
+    actionsEl.classList.add("actions");
+
+    const editBtnEl = document.createElement("button");
+    editBtnEl.classList.add("material-icons");
+    editBtnEl.innerText = "edit";
+
+    const removeBtnEl = document.createElement("button");
+    removeBtnEl.classList.add("material-icons", "remove-btn");
+    removeBtnEl.innerText = "remove_circle";
+
+    // 요소 노드에 추가
+    actionsEl.append(editBtnEl);
+    actionsEl.append(removeBtnEl);
+
+    itemEl.append(checkbox);
+    itemEl.append(inputEl);
+    itemEl.append(actionsEl);
+
+    // 이벤트 생성
+    checkbox.addEventListener("change", () => {
+        item.complete = checkbox.checked;
+
+        if (item.complete) {
+            itemEl.classList.add("complete");
+        } else {
+            itemEl.classList.remove("complete");
+        }
+
+        saveToLocalStorage();
+    });
+
+    inputEl.addEventListener("input", () => {
+        item.text = inputEl.value;
+    });
+
+    inputEl.addEventListener("blur", () => {
+        inputEl.setAttribute("disabled", "");
+
+        saveToLocalStorage();
+    });
+
+    editBtnEl.addEventListener("click", () => {
+        inputEl.removeAttribute("disabled");
+        inputEl.focus();
+    });
+    
+    removeBtnEl.addEventListener("click", () => {
+        todos = todos.filter(t => t.id != item.id);
+        itemEl.remove();
+
+        saveToLocalStorage();
+    });
+
+    return { itemEl, inputEl, editBtnEl, removeBtnEl }
+
+}
+
+function displayTodos() {
+    loadFromLocalStorage();
+
+    for (let i = 0; i < todos.length; i++){
+        const item = todos[i];
+
+        const { itemEl } = createTodoElment(item);
+
+        list.append(itemEl);
+    }
+}
+
+displayTodos();
+
+function saveToLocalStorage() {
+    const data = JSON.stringify(todos);
+
+    localStorage.setItem("my_todos", data);
+
+}
+
+function loadFromLocalStorage() {
+    const data = localStorage.getItem("my_todos");
+
+    if(data){
+        todos = JSON.parse(data);
+    }
+}
